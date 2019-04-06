5319675034fe9d52caddaf01a04af6129153109a
K-Yonhon <k.yonhon@gmail.com>
2019-04-06 18:03:33 +0900

    Rebuild GAN by using CNN

--- pix2pix/download_dataset.sh
+++ pix2pix/download_dataset.sh
@@ -1,3 +1,12 @@
+mkdir -p datasets
+URL=https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/
+wget -N -r -l 1 -w 1 -nd -nH -np -erobots=off -A tar.gz $URL -P ./datasets/
+for FILE in `find $1 -name "*.tar.gz"`; do
+tar -zxvf $FILE -C ./datasets/
+rm $FILE
+done
+
+<< COMMENTOUT
 mkdir datasets
 FILE=$1
 URL=https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/$FILE.tar.gz
@@ -7,3 +16,4 @@ wget -N $URL -O $TAR_FILE
 mkdir $TARGET_DIR
 tar -zxvf $TAR_FILE -C ./datasets/
 rm $TAR_FILE
+COMMENTOUT
